82d92d330efeb83bf9789f349248f00bd40c26d5
Amirdhani S <amirdhanis@gmail.com>
2025-06-14 14:38:19 +0530

    analytic controller spelling mistake in export const getAnalyticsData

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -2,7 +2,7 @@ import Order from "../models/order.model.js";
 import Product from "../models/product.model.js";
 import User from "../models/user.model.js";
 
-export const getAnalayticsData = async() => {
+export const getAnalyticsData = async() => {
     const totalUsers = await User.countDocuments();
     const totalProducts = await Product.countDocuments();
 
